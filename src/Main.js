895d7907bd034e4c6f8288a07dda4dd26b21ed50
Reto Gmür <reto@factsmission.com>
2020-12-27 14:13:03 +0100

    allowing to set limit

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import './TypeSelector.js'
 import './AuthorSelector'
+import '@vaadin/vaadin-text-field/vaadin-number-field.js'
 
 root.innerHTML = `
 <h1>Scuttlebutt Feedviewer</h1>
@@ -7,24 +8,30 @@ root.innerHTML = `
 <type-selector></type-selector>
 <author-selector></author-selector>
 
+<vaadin-number-field label="limit" value="100" min="1" 
+step="10" has-controls></vaadin-number-field>
+
 <button class="query">Query</button>
 
 <div class="output"></div>
 `
 const typeSelector = root.getElementsByTagName('type-selector')[0]
 const authorSelector = root.getElementsByTagName('author-selector')[0]
+const limitSelector = root.getElementsByTagName('vaadin-number-field')[0]
 const outputArea = root.getElementsByClassName('output')[0]
 const queryButton = root.getElementsByClassName('query')[0]
 
 queryButton.addEventListener('click', function (event) {
   const typeFilter = typeSelector.value !== '' ? typeSelector.value : undefined
   const authorFilter = authorSelector.value !== '' ? authorSelector.value : undefined
+  const limit = limitSelector.value !== '' ? Number.parseInt(limitSelector.value) : undefined
   outputArea.innerHTML = `<div id="header">
-  type: ${typeFilter ? typeFilter : '<i>all types</i>'}, author: ${authorFilter ? authorFilter : '<i>any author</i>'}
+  type: ${typeFilter ? typeFilter : '<i>all types</i>'}, author: ${authorFilter ? authorFilter : '<i>any author</i>'},
+  limit: ${limit}
   </div>`
 
   const opts = {
-    limit: 100,
+    limit,
     reverse: true,
     query: [
       {
